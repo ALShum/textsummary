877fcd4baa37b34c685ec433e069fc0ff7a84b97
RobertCorell <robert.w.corell@gmail.com>
2014-11-24 19:14:00 -0600

    implemented the OpenNLPSentenceProcessor

--- src/OpenNLPSentenceProcessor.java
+++ src/OpenNLPSentenceProcessor.java
@@ -1,4 +1,16 @@
+import java.io.FileInputStream;
+import java.io.InputStream;
 import java.util.AbstractList;
+import java.util.ArrayList;
+import java.util.Hashtable;
+
+import opennlp.tools.postag.POSModel;
+import opennlp.tools.postag.POSTaggerME;
+import opennlp.tools.sentdetect.SentenceDetectorME;
+import opennlp.tools.sentdetect.SentenceModel;
+import opennlp.tools.tokenize.Tokenizer;
+import opennlp.tools.tokenize.TokenizerME;
+import opennlp.tools.tokenize.TokenizerModel;
 
 /**
  * Implementation of ISentenceProcessor that uses the OpenNLP library as a backbone.
@@ -7,16 +19,57 @@ import java.util.AbstractList;
  */
 public class OpenNLPSentenceProcessor implements ISentenceProcessor {
 
+	Hashtable<String, Boolean> tags;
+	
+	public OpenNLPSentenceProcessor() {
+		tags= new Hashtable<String, Boolean>();
+		for(String s : new String[]{"$", "``","''", "(", ")", ",", "--", ".", ":", "CC", "CD", "DT", "EX", "FW", "IN", "JJ", "JJR", "JJS", "LS", "MD", "NN", "NNP", "NNPS", "NNS", "PDT", "POS", "PRP", "PRP$", "RB", "RBR", "RBS", "RP", "SYM", "TO", "UH", "VB", "VBD", "VBG", "VBN", "VBP", "VBZ", "WDT", "WP", "WP$","WRB"}) {
+			tags.put(s, true);
+		}
+	}
+	
 	@Override
 	public AbstractList<String> SeparateSentences(String text) {
 		// Use Sentence Detection API
-		return null;
+		SentenceModel model = null;
+		try{
+			InputStream modelIn = new FileInputStream("en-sent.bin");
+			model = new SentenceModel(modelIn);
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		SentenceDetectorME sentenceDetector = new SentenceDetectorME(model);
+		String[] sentences = sentenceDetector.sentDetect(text);
+		ArrayList<String> list = new ArrayList<String>();
+		for (String s : sentences) {
+			list.add(s);
+		}
+		return list;
 	}
 
 	@Override
 	public AbstractList<String> TokenizeAndStripSentence(String sentence) {
 		// Use Tokenize API, then Part-of-Speech API, then remove unnecessary words based on tag
-		return null;
+		ArrayList<String> list = new ArrayList<String>();
+		TokenizerModel tmodel = null;
+		POSModel pmodel=null;
+		try{
+			InputStream tmodelIn = new FileInputStream("en-token.bin");
+			InputStream pmodelIn = new FileInputStream("en-pos-maxent.bin");
+			tmodel = new TokenizerModel(tmodelIn);
+			pmodel= new POSModel(pmodelIn);
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		Tokenizer tokenizer = new TokenizerME(tmodel);
+		POSTaggerME tagger = new POSTaggerME(pmodel);
+		
+		String[] sent = tokenizer.tokenize(sentence);
+		String[] tag =tagger.tag(sent);
+		for(int i=0; i<sent.length;i++) {
+			if(tags.containsKey(tag[i]) && tags.get(tag[i])) list.add(sent[i]);
+		}
+		return list;
 	}
 
 	/**
@@ -25,8 +78,10 @@ public class OpenNLPSentenceProcessor implements ISentenceProcessor {
 	 * @see http://www.cis.upenn.edu/~treebank/
 	 */
 	public void SetRelevantTokenTags(String[] relevantTokenTags) {
-		// TODO Auto-generated method stub
-		
+		tags.clear();
+		for(String s : relevantTokenTags) {
+			tags.put(s, true);
+		}
 	}
 
 }
