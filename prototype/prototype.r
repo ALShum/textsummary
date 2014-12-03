0d541c60d24bdd3d9546be5ebe436c9a094ce422
Alex Shum <ashum86@gmail.com>
2014-12-03 15:22:22 -0600

    included R prototype file

---
+++ prototype/prototype.r
@@ -0,0 +1,127 @@
+library(openNLP)
+library(NLP)
+
+#http://www.huffingtonpost.com/nell-minow/jim-davis-explains-why-ga_b_6094892.html?utm_hp_ref=funny
+text = "Cartoonist Jim Davis is the man behind lasagna-loving, Monday-hating Garfield the cat, who lives with his long-suffering owner, Jon, and fellow pet and perennial crossword puzzle clue, Odie the dog. 'Garfield' is the world's most widely syndicated comic strip, with dozens of record-breaking best-selling books, plus award-winning animated television series and specials and more than 400 licensed products including everything from toys, games, backpacks, and costumes to bedding, credit cards, and diapers. This week, Garfield's Holiday Collection has a new DVD release. It includes the special Davis calls 'the most 'Garfield,' in that it involves food.' It is the Thanksgiving show, a special challenge because the holiday feast arrives just as Garfield goes on a diet.
+
+In an interview, Davis talked about the comic strips he loved as a kid growing up on a farm in the midwest. He loved Milton Caniff's Steve Canyon, especially the Dragon Lady. And 'I was a big fan of Peanuts and Beetle Bailey, Hi and Lois, eventually B.C. and Wizard of Oz and also Pogo. That one, I never understood it but I'd love looking at the pictures, the artwork by Walt Kelly.'
+
+Davis enjoys producing Garfield in all forms of media, but 'what gets me out every day is the comic strip. That's what I do best and I love it. It is so much fun to do because it is really positive just sitting and thinking of funny stuff and then trying to draw a really funny picture. Here I am a grown man, still it is so much fun.' But he has very fond memories of working on the specials as well, especially because they were done before computer animation, 'the old-fashioned way,' each one taking about eight months to animate. 'They are just wonderful stories and I enjoy the memories of that time and the kind of faith that I think the network and producers and everyone had in our operation as far as being able to do the scripts and direct voice tracks and do the storyboards.'
+
+Lorenzo Music provided the voice for Garfield. 'He had a way of throwing a line away and not really caring about it. Everything was said with such comedic timing and made you laugh. That's very rare. Especially back in the day when comedians were pretty much over the top, pretty much stand up kind of timing and stuff like that. He was really a departure, and he captured the spirit of Garfield so perfectly.'
+
+Like Garfield, Davis is a fan of lasagne. 'Yeah, I love lasagne. I thought it would be funny to have a cat who likes lasagne but as it turns out, I hear from people all the time that their cats love lasagne.' And he confessed that even though Jon loves Odie, too, Garfield is his favorite 'because of the depth of their relationship. He confides in Garfield so much. and I think Jon may have a little masochistic problem. He enjoys the abuse that Garfield heaps up.'
+
+I have always wondered why Garfield hates Mondays when every day is pretty much the same for him, but Davis had the answer. 'Garfield does not have a job, Garfield does not go to school and every day is the same. Nevertheless every Monday is just a reminder that his life is the same old, same old cycling again and for some reason even though his life is pretty much the same every day on Mondays specifically, awful things tend to happen to him physically.'
+
+Garfield's widespread appeal is as a sort of feline everyman who experiences what we do but says what we do not. 'The very first products done had lines like I Hate Mondays, I'd Like Mondays Better If It Started Later, and things people did not want to say themselves but they would let Garfield say it for them. It is the attitude that people gravitate to because he is such a great escape for that.'
+
+Garfield is also there to let us know that he is on our side. 'We live in a time where we are made to feel guilty for not exercising and oversleeping and overeating, but Garfield's cool with that. He likes it all and he is not apologetic about it. He just lays it out there the way it is. I think that is what people really appreciate about him because he kind of holds the mirror back to the reader. He throws it back with a humorous twist."
+
+text = "US-led air raids have struck Syrian rebels not linked to the Islamic State of Iraq and the Levant (ISIL), expanding the coalitions raids for the second time to other groups fighting in Syria.
+
+Activists in Syria told Al Jazeera the raids struck the Ahrar al-Sham rebel group in the northern province of Idlib, and the Syrian branch of al-Qaeda, the Nusra Front, in western Aleppo on Thursday.
+
+At least six coalition air raids struck Idlib overnight, with one targeting an Ahrar al-Sham position in the village of Babska.
+
+One of Syria's largest armed opposition groups, Ahrar al-Sham is a key member of the Islamic Front coalition, which seeks a state run on Islamic principles as its fights to topple Syrian President Bashar al-Assad.
+
+
+Syria raids: Widening the war on ISIL
+Air raids also targeted the Nusra Front in the Reef al-Muhandisin area of western Aleppo, reportedly killing several of the groups' fighters as well as six civilians.
+
+The Nusra Front, which has several bases in the area, confirmed the strikes on Twitter, saying the raids caused a high number of civilian casualties.
+
+Al Jazeera could not independently confirm the number of casualties.
+
+Meanwhile, Al Jazeera also learned that one of the air strikes killed David Drugeon, a French national, who is identified as a skilled bomb expert for Nusra. The strike targeted Drugeon while he was in his car in Sarmada near the Turkey-Syrian border.   
+
+The US and Arab states have been carrying out air strikes against ISIL in Syria since September 23, in a bid to prevent the armed group from seizing more territory in the conflict-hit nation.
+
+Targeting Khorasan
+
+On the first day of the attacks, the US-led coalition also hit a group of al-Qaeda veterans that the US administration called 'Khorasan', although analysts said the attack actually targeted the Nusra Front.
+
+There have been no reported attacks on groups outside ISIL since, but the latest air raids come after the group made gains against Western-backed rebel fighters in the Idlib region.
+
+Nusra fighters in the past week drove the Syrian Revolutionary Front from its bastion in Idlib and captured a town after the withdrawal of the Hazm movement, another moderate opposition group.
+
+The three-year war in Syria has killed an estimated 200,000 people and divided the country.
+
+Rebel groups fighting the regime have taken over large swathes of land while ISIL holds territory in the north and east. 
+
+Nearly 10 million people have been displaced by Syria's civil war, and an estimated three million refugees have fled the country, according to the UN."
+
+text = "A 9,300-year-old frozen bison mummy has been found in Eastern Siberia, according to a presentation this week at the Society of Vertebrate Paleontology’s Annual Meeting in Berlin.
+
+The still-furry beast is one of the most complete frozen mummies ever found. It literally freezes in time the appearance and anatomy of a steppe bison (Bison priscus), whose species went extinct shortly after the end of the Ice Age.
+
+Mummies Faces,Hair-dos, Revealed in 3D: Photos
+
+It’s been named the Yukagir bison mummy, after the region where it was found.
+
+“The exceptionally good preservation of the Yukagir bison mummy allows direct anatomical comparisons with modern species of bison and cattle, as well as with extinct species of bison that were gone at the Pleistocene-Holocene boundary,” co-author Evgeny Maschenko from the Paleontological Institute in Moscow was quoted as saying in a press release.
+
+The remarkable specimen still has its complete brain, heart, blood vessels and digestive system. Some of its organs have significantly shrunk over time, but that’s to be expected given its advanced age.
+
+Video: Three Extinct Animals Making a Comeback
+
+The researchers, led by Natalia Serduk of the Russian Academy of Sciences in Moscow, conducted a necropsy on the remains. The investigation determined that the bison showed a relatively normal anatomy. A clue to its demise, however, is a lack of fat around its abdomen. This suggests that the bison died from starvation, but the scientists aren’t sure of that yet.
+
+Compared to today’s bison in America, the Ice Age bison sported much larger horns and a second back hump. Steppe bison like this now-frozen one were commonly featured in Stone Age cave art, often shown being hunted by humans.
+
+Remains for a woolly rhino, a 35,000–39,000-year-old horse, and a mammoth were also recently found near the Siberian site where the bison mummy was discovered.
+
+Could These 10 Animals Be Resurrected?
+
+The next steps to be done include further examination of the bison’s gross anatomy, and other detailed studies on its histology, parasites, and bones and teeth, co-author Olga Potapova said.
+
+She added, We expect that the results of these studies will reveal not only the cause of death of this particular specimen, but also might shed light on the species behavior and causes of its extinction."
+
+text = as.String(text)
+sent_token_annotator <- Maxent_Sent_Token_Annotator()
+sentences = annotate(text, list(sent_token_annotator))
+sentence_list = text[sentences]
+
+#does not remove stop words
+distance = function(sent1, sent2) {
+	#lower case
+	sent1 = tolower(sent1)
+	sent2 = tolower(sent2)
+	
+	#remove punctuation
+	sent1 = gsub("[[:punct:]]", "", as.character(sent1))
+	sent2 = gsub("[[:punct:]]", "", as.character(sent2))
+
+	#split into sets
+	sent1 = strsplit(sent1, " ")[[1]]
+	sent2 = strsplit(sent2, " ")[[1]]
+	both = intersect(sent1, sent2)
+	
+	return(length(both) / (log(length(sent1)) + log(length(sent2))))
+}
+
+adj_matrix = function(sentence_list) {
+	n = length(sentence_list)
+	adj = matrix(NA, nrow = n, ncol = n)
+	
+	for(i in 1:n) {
+		for(j in 1:n) {
+			adj[i,j] = distance(sentence_list[i], sentence_list[j])
+		}
+	}
+	return(adj)
+}
+
+adj = adj_matrix(sentence_list)
+vec = rep(1/17, 17)
+vec2 = vec
+
+adj = apply(adj, 2, function(x) {
+	y = sum(x)
+	x = x / y
+})
+
+for(i in 1:1000) {
+	vec2 = 0.85 * adj %*% vec2 + (0.15)
+}
